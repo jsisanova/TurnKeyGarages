c93f546808b9132dbd6e26012b5a074174532da8
jsisanova <jana.sisanova@gmail.com>
2018-12-10 16:31:17 +0100

    Store text strings in strings.xml

---
+++ app/src/main/java/com/example/android/turnkeygarages/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.turnkeygarages.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.turnkeygarages.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
